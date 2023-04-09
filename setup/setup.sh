b684be86446dbbd42806cc83484bec3546fb7f1f
Kazuya Yamamoto <konkoro0503@gmail.com>
2023-04-09 17:19:57 +0900

    set up with venv

--- setup/setup.sh
+++ setup/setup.sh
@@ -15,39 +15,26 @@ AKARI_DOC_PARENT_PATH=`pwd | xargs dirname`
 titleEcho "apt update"
 apt_check_and_update
 
-
-titleEcho " Install curl"
+titleEcho "Install curl"
 apt_check_and_install curl
 
-titleEcho "Install python3-pip" 
-apt_check_and_install python3-pip
 
-titleEcho " Install poetry"
-curl -sSL https://install.python-poetry.org | python3 -
-successEcho "poetry installed"
-
-titleEcho "Add poetry path in .bashrc"
-if grep 'export PATH="\$HOME/.poetry/bin:\$PATH"' ~/.bashrc > /dev/null 2>&1; then 
- skipEcho "poetry path has already been available in .bashrc."
-else
- echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
- successEcho "poetry path was added in .bashrc"
-fi
-
-titleEcho "Install pipenv"
-pip3_check_and_install pipenv
-
-titleEcho "set up akari poetry env"
+titleEcho "Create and set up venv"
 (
-    source $HOME/.poetry/env
-    cd $AKARI_DOC_PARENT_PATH
-    poetry install
+  cd $AKARI_DOC_PARENT_PATH
+  python -m venv venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+  successEcho "venv set up finished"
 )
 
-titleEcho " Build document"
-cd $AKARI_DOC_PARENT_PATH
-poetry run make html
-successEcho "Document built."
+titleEcho "Build document"
+(
+  cd $AKARI_DOC_PARENT_PATH
+  source venv/bin/activate
+  make html
+  successEcho "Document built."
+)
 
 echo -e " "
 echo -e "\e[32;1m----------------------------------------\e[m"
