d9ecae34ed80120410244ba06341f7d68ccb1792
Kazuya Yamamoto <konkoro0503@gmail.com>
2023-04-11 22:37:28 +0900

    set up with venv (#78)

--- setup/build.sh
+++ setup/build.sh
@@ -12,10 +12,13 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 AKARI_DOC_PARENT_PATH=`pwd | xargs dirname`
 . $AKARI_DOC_PARENT_PATH/setup/lib/common.bash    # 共通の関数や定数をライブラリからロード
 
-titleEcho " Build document"
-cd $AKARI_DOC_PARENT_PATH
-poetry run make html
-successEcho "Document built."
+titleEcho "Build document"
+(
+  cd $AKARI_DOC_PARENT_PATH
+  source venv/bin/activate
+  make html
+  successEcho "Document built."
+)
 
 echo -e " "
 echo -e "\e[32;1m----------------------------------------\e[m"
